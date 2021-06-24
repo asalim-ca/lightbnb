deaf139e1ad9d1b50805d0ed2cff3bec744221ca
Ali Salim <alisalim.ca@gmail.com>
2021-06-24 23:46:14 +0000

    LighthouseBnB Users

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -31,7 +31,10 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+  return pool
+    .query(`SELECT * FROM users WHERE id = $1`, [id])
+    .then(result => result.rows[0])
+    .catch(err => console.log(err.message));
 }
 exports.getUserWithId = getUserWithId;
 
@@ -42,9 +45,13 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
+  pool
+    .query(`
+    INSERT INTO users (name, email)
+    VALUES ($1, $2) RETURNING *
+    `, [user.name, user.email])
+    .then(result => result.rows[0])
+    .catch(err => console.log(err.message));
   return Promise.resolve(user);
 }
 exports.addUser = addUser;
