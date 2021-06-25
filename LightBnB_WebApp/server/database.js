b706215a90fb8a117d4e398a040a8ea06fe25dd9
Ali Salim <alisalim.ca@gmail.com>
2021-06-25 02:17:23 +0000

    MUST review minimum_rating

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -86,10 +86,64 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
  const getAllProperties = (options, limit = 10) => {
-  return pool
-    .query(`SELECT * FROM properties LIMIT $1`, [limit])
-    .then(result => Object.assign({}, result.rows))
-    .catch(err => console.log(err.message));
+  // 1
+  const queryParams = [];
+  // 2
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  `;
+
+  // 3
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `WHERE city LIKE $${queryParams.length} `;
+  }
+
+  if(options.owner_id) {
+    queryParams.push(options.owner_id);
+    queryString += `JOIN users ON users.id = owner_id WHERE owner_id = $${queryParams.length} `;
+  }
+
+  if (options.minimum_price_per_night && !options.maximum_price_per_night) {
+    queryParams.push(options.minimum_price_per_night * 100);
+    queryString += `WHERE properties.cost_per_night >= $${queryParams.length} `;
+  }
+  if (!options.minimum_price_per_night && options.maximum_price_per_night) {
+    queryParams.push(options.maximum_price_per_night * 100);
+    queryString += `WHERE properties.cost_per_night <= $${queryParams.length} `;
+  }
+
+  if (options.minimum_price_per_night && options.maximum_price_per_night) {
+    queryParams.push(options.minimum_price_per_night * 100);
+    queryParams.push(options.maximum_price_per_night * 100);
+    queryString += `WHERE properties.cost_per_night >= $${queryParams.length-1} AND properties.cost_per_night <= $${queryParams.length} `;
+  }
+
+  if(options.minimum_rating && !(options.minimum_price_per_night || options.maximum_price_per_night)) {
+    queryParams.push(options.minimum_rating);
+    queryString += `HAVING property_reviews.rating >= $${queryParams.length} `;
+  }
+
+  if(options.minimum_rating && (options.minimum_price_per_night || options.maximum_price_per_night)) {
+    queryParams.push(Number(options.minimum_rating));
+    queryString += `AND property_reviews.rating >= $${queryParams.length} `;
+  }
+
+  // 4
+  queryParams.push(limit);
+  queryString += `
+  GROUP BY properties.id
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+
+  // 5
+  console.log(queryString, queryParams);
+
+  // 6
+  return pool.query(queryString, queryParams).then((res) => res.rows);
 };
 exports.getAllProperties = getAllProperties;
 
